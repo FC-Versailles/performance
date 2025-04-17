a5257a058d3b7b61a4918bc6712096b8e71d679d
FCV <club@fcversailles.com>
2025-04-17 21:13:08 +0200

    Add files via upload

---
+++ pages/6_Game_Analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 17 21:11:52 2025
+
+@author: fcvmathieu
+"""
+
---
+++ pages/8_Team_Analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 17 21:11:53 2025
+
+@author: fcvmathieu
+"""
+
