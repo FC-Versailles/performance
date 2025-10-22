b00d525c550beaffe6ebd3a92a6b55088f4a664f
FCV <club@fcversailles.com>
2025-10-22 14:50:03 +0200

    Add files via upload

--- pages/9_Scout.py
+++ pages/9_Scout.py
@@ -79,11 +79,11 @@ comp = sb.competitions(creds = DEFAULT_CREDS)
 sb.CACHE_ENABLED = False  
 session = requests_cache.CachedSession(backend="memory")
 
-df1 = sb.player_season_stats(competition_id=129, season_id=317,creds = DEFAULT_CREDS)
-df2 = sb.player_season_stats(competition_id=1035, season_id=317,creds = DEFAULT_CREDS)
-df3 = sb.player_season_stats(competition_id=8, season_id=317,creds = DEFAULT_CREDS)
-df4 = sb.player_season_stats(competition_id=177, season_id=317,creds = DEFAULT_CREDS)
-df5 = sb.player_season_stats(competition_id=63, season_id=317,creds = DEFAULT_CREDS)
+df1 = sb.player_season_stats(competition_id=129, season_id=318,creds = DEFAULT_CREDS)
+df2 = sb.player_season_stats(competition_id=1035, season_id=318,creds = DEFAULT_CREDS)
+df3 = sb.player_season_stats(competition_id=8, season_id=318,creds = DEFAULT_CREDS)
+df4 = sb.player_season_stats(competition_id=177, season_id=318,creds = DEFAULT_CREDS)
+df5 = sb.player_season_stats(competition_id=63, season_id=318,creds = DEFAULT_CREDS)
 
 
 data = pd.concat([df1, df2,df3,df4,df5], ignore_index=True)
